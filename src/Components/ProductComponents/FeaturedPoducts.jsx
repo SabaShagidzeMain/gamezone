50c636aa78af48a772e82f078ee8a55eae09b59e
SabaShagidze2 <sabadoeswebdev@gmail.com>
2024-10-19 23:03:32 +0400

    Styling, Optimization, Structuring

--- src/Components/ProductComponents/FeaturedPoducts.jsx
+++ src/Components/ProductComponents/FeaturedPoducts.jsx
@@ -3,57 +3,13 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import styles from "./FeaturedProducts.module.css";
-
-const featuredGames = [
-  {
-    id: 1,
-    title: "Warhammer: Space Marine 2",
-    logo: "/images/featured/logo/logo-space-marine.webp",
-    background: "/images/featured/background/bck-space-marine.jpeg",
-    thumbnail: "/images/featured/thumbnail/thmb-space-marine.jpeg",
-    desc: "Warhammer 40,000: Space Marine 2 on Steam. Embody the superhuman skill and brutality of a Space Marine. Unleash deadly abilities and devastating weaponry to obliterate the relentless Tyranid swarms. Defend the Imperium in spectacular third-person action in solo or multiplayer modes.",
-  },
-  {
-    id: 2,
-    title: "Black Myth: Wu-kong",
-    logo: "/images/featured/logo/logo-wukong.png",
-    background: "/images/featured/background/bck-wukong.jpg",
-    thumbnail: "/images/featured/thumbnail/thmb-wukong.jpg",
-    desc: "Black Myth: Wukong is an action role-playing game. It has elements characteristic to the Soulslike subgenre. The game is played in single-player mode from a third-person perspective. The player controls a monkey protagonist referred to as the Destined One, who is based on Sun Wukong from the Journey to the West.",
-  },
-  {
-    id: 3,
-    title: "CyberPunk 2077",
-    logo: "/images/featured/logo/logo-cyberpunk.png",
-    background: "/images/featured/background/bck-cyberpunk.jpg",
-    thumbnail: "/images/featured/thumbnail/thmb-cyberpunk.jpg",
-    desc: "Star Wars Outlaws is an open world video game developed by Massive Entertainment and published by Ubisoft in collaboration with Lucasfilm Games. The game is story-driven, features an open world, and utilizes Massive Entertainment's proprietary Snowdrop game engine.",
-  },
-  {
-    id: 4,
-    title: "Marvel: Spiderman 2",
-    logo: "/images/featured/logo/logo-spiderman.png",
-    background: "/images/featured/background/bck-spiderman.jpg",
-    thumbnail: "/images/featured/thumbnail/thmb-spiderman.jpg",
-    desc: "Warhammer 40,000: Space Marine 2 on Steam. Embody the superhuman skill and brutality of a Space Marine. Unleash deadly abilities and devastating weaponry to obliterate the relentless Tyranid swarms. Defend the Imperium in spectacular third-person action in solo or multiplayer modes.",
-  },
-  {
-    id: 5,
-    title: "Ghost Of Tsushima",
-    logo: "/images/featured/logo/logo-ghost.png",
-    background: "/images/featured/background/bck-ghost.jpg",
-    thumbnail: "/images/featured/thumbnail/thmb-ghost.jpg",
-    desc: "Warhammer 40,000: Space Marine 2 on Steam. Embody the superhuman skill and brutality of a Space Marine. Unleash deadly abilities and devastating weaponry to obliterate the relentless Tyranid swarms. Defend the Imperium in spectacular third-person action in solo or multiplayer modes.",
-  },
-];
+import featuredGames from "@/utilities/featuredGames/featuredGames";
 
 const FeaturedProducts = () => {
   const [activeGame, setActiveGame] = useState(featuredGames[0]);
 
-  // Destructure activeGame
   const { logo, title, desc, background } = activeGame;
 
-  // Function to handle image loading errors
   const handleImageError = (gameTitle) => {
     console.error(`Failed to load logo for: ${gameTitle}`);
   };
@@ -77,7 +33,6 @@ const FeaturedProducts = () => {
           <button className={styles.button}>გაიგე მეტი</button>
         </div>
       </div>
-
       <div className={styles.thumbnails}>
         {featuredGames.map((game) => (
           <div
