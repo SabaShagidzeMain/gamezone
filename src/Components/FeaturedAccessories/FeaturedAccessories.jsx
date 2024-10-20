9285ee18d63b8b469c0508601000b923987d25c0
SabaShagidzeMain <sabadoeswebdev@gmail.com>
2024-10-20 13:13:15 +0400

    Optimization, Naming Changes

--- src/Components/FeaturedAccessories/FeaturedAccessories.jsx
+++ src/Components/FeaturedAccessories/FeaturedAccessories.jsx
@@ -4,6 +4,19 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 
+const accessories = [
+  {
+    name: "DualSense Wireless Controller",
+    description: "Discover A Deeper, Highly Immersive Gaming Experience",
+    image: "",
+  },
+  {
+    name: "Pulse 3D Wireless Headset",
+    description: "Enjoy A Seamless, Wireless Audio Experience,",
+    image: "",
+  },
+];
+
 const FeaturedAccessories = () => {
   return <div>FeaturedAccessories</div>;
 };
