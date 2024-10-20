e8c1472af3738002c1beaf21a26d5a677febedab
SabaShagidze2 <sabadoeswebdev@gmail.com>
2024-10-20 21:54:47 +0400

    Pulled Featured Accessories Arr Outside
    to a file in utilities named featuredAcessoriesArr.js

--- src/Components/FeaturedAccessories/FeaturedAccessories.jsx
+++ src/Components/FeaturedAccessories/FeaturedAccessories.jsx
@@ -7,127 +7,64 @@ import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import styles from "./FeaturedAccessories.module.css";
 import Image from "next/image";
-
-const accessories = [
-  {
-    id: 1,
-    name: "DualSense Wireless Controller",
-    description: "Discover A Deeper, Highly Immersive Gaming Experience",
-    image:
-      "/images/featuredAccessories/dualsense-edge-featured-hardware-image-block-01-en-11aug23.webp",
-  },
-  {
-    id: 2,
-    name: "Pulse 3D Wireless Headset",
-    description: "Enjoy A Seamless, Wireless Audio Experience,",
-    image:
-      "/images/featuredAccessories/hd-camera-featured-hardware-image-block-01-en-11aug23.webp",
-  },
-  {
-    id: 3,
-    name: "Pulse 3D Wireless Headset",
-    description: "Enjoy A Seamless, Wireless Audio Experience,",
-    image:
-      "/images/featuredAccessories/PULSE-Elite-headset-thumbnail-01-en-08sep23.webp",
-  },
-  {
-    id: 4,
-    name: "Pulse 3D Wireless Headset",
-    description: "Enjoy A Seamless, Wireless Audio Experience,",
-    image:
-      "/images/featuredAccessories/PULSE-Elite-headset-thumbnail-01-en-08sep23.webp",
-  },
-  {
-    id: 5,
-    name: "DualSense Wireless Controller",
-    description: "Discover A Deeper, Highly Immersive Gaming Experience",
-    image:
-      "/images/featuredAccessories/dualsense-edge-featured-hardware-image-block-01-en-11aug23.webp",
-  },
-  {
-    id: 6,
-    name: "Pulse 3D Wireless Headset",
-    description: "Enjoy A Seamless, Wireless Audio Experience,",
-    image:
-      "/images/featuredAccessories/hd-camera-featured-hardware-image-block-01-en-11aug23.webp",
-  },
-  {
-    id: 7,
-    name: "Pulse 3D Wireless Headset",
-    description: "Enjoy A Seamless, Wireless Audio Experience,",
-    image:
-      "/images/featuredAccessories/PULSE-Elite-headset-thumbnail-01-en-08sep23.webp",
-  },
-  {
-    id: 8,
-    name: "Pulse 3D Wireless Headset",
-    description: "Enjoy A Seamless, Wireless Audio Experience,",
-    image:
-      "/images/featuredAccessories/PULSE-Elite-headset-thumbnail-01-en-08sep23.webp",
-  },
-];
+import featuredAccessoriesArr from "@/utilities/featuredAccessoriesArr/featuredAccessoriesArr";
 
 const FeaturedAccessories = () => {
-  const [selectedAccessory, setSelectedAccessory] = useState(accessories[0]);
+  const [selectedAccessory, setSelectedAccessory] = useState(
+    featuredAccessoriesArr[0]
+  );
 
   const handleProductClick = (accessory) => {
     setSelectedAccessory(accessory);
   };
 
+  const { name, description, image: selectedImage } = selectedAccessory;
+
   return (
-    <>
-      <div className={styles.accessories_section}>
-        <div className={styles.selected_product}>
-          <div className={styles.selected_product_info}>
-            <h2 className={styles.selected_product_header}>
-              Discover All Accessories
-            </h2>
-            <h3 className={styles.selected_product_name}>
-              {selectedAccessory.name}
-            </h3>
-            <p className={styles.selected_product_desc}>
-              {selectedAccessory.description}
-            </p>
-            <div className={styles.selected_product_button_container}>
-              <button className={styles.selected_product_button}>გაიგე მეტი</button>
-              <button className={styles.selected_product_button}>შეძენა</button>
-            </div>
-          </div>
-          <div className={styles.selected_product_image}>
-            <Image
-              src={selectedAccessory.image}
-              alt={selectedAccessory.name}
-              width={800}
-              height={450}
-            />
+    <div className={styles.accessories_section}>
+      <div className={styles.selected_product}>
+        <div className={styles.selected_product_info}>
+          <h2 className={styles.selected_product_header}>
+            Discover All Accessories
+          </h2>
+          <h3 className={styles.selected_product_name}>{name}</h3>
+          <p className={styles.selected_product_desc}>{description}</p>
+          <div className={styles.selected_product_button_container}>
+            <button className={styles.selected_product_button}>
+              გაიგე მეტი
+            </button>
+            <button className={styles.selected_product_button}>შეძენა</button>
           </div>
         </div>
-
-        <Swiper
-          className={styles.accessory_swiper}
-          spaceBetween={5}
-          slidesPerView={6}
-          navigation={true}
-          modules={[Navigation]}
-        >
-          {accessories.map((accessory, index) => (
-            <SwiperSlide
-              key={index}
-              onClick={() => handleProductClick(accessory)}
-            >
-              <div className={styles.accessory_card}>
-                <Image
-                  src={accessory.image}
-                  alt={accessory.name}
-                  width={150}
-                  height={80}
-                />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <div className={styles.selected_product_image}>
+          <Image src={selectedImage} alt={name} width={800} height={450} />
+        </div>
       </div>
-    </>
+
+      <Swiper
+        className={styles.accessory_swiper}
+        spaceBetween={5}
+        slidesPerView={6}
+        navigation={true}
+        modules={[Navigation]}
+      >
+        {featuredAccessoriesArr.map((accessory) => (
+          <SwiperSlide
+            key={accessory.id}
+            onClick={() => handleProductClick(accessory)}
+          >
+            <div className={styles.accessory_card}>
+              <Image
+                src={accessory.image}
+                alt={accessory.name}
+                width={150}
+                height={80}
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
---
+++ src/utilities/featuredAccessoriesArr/featuredAccessoriesArr.js
@@ -0,0 +1,74 @@
+const featuredAccessoriesArr = [
+  {
+    id: 1,
+    name: "DualSense Wireless Controller",
+    description: "Discover A Deeper, Highly Immersive Gaming Experience",
+    image:
+      "/images/featuredAccessories/dualsense-edge-featured-hardware-image-block-01-en-11aug23.webp",
+  },
+  {
+    id: 2,
+    name: "Pulse 3D Wireless Headset",
+    description: "Enjoy A Seamless, Wireless Audio Experience",
+    image:
+      "/images/featuredAccessories/hd-camera-featured-hardware-image-block-01-en-11aug23.webp",
+  },
+  {
+    id: 3,
+    name: "Pulse 3D Wireless Headset",
+    description: "Enjoy A Seamless, Wireless Audio Experience",
+    image:
+      "/images/featuredAccessories/PULSE-Elite-headset-thumbnail-01-en-08sep23.webp",
+  },
+  {
+    id: 4,
+    name: "Pulse 3D Wireless Headset",
+    description: "Enjoy A Seamless, Wireless Audio Experience",
+    image:
+      "/images/featuredAccessories/PULSE-Elite-headset-thumbnail-01-en-08sep23.webp",
+  },
+  {
+    id: 5,
+    name: "DualSense Wireless Controller",
+    description: "Discover A Deeper, Highly Immersive Gaming Experience",
+    image:
+      "/images/featuredAccessories/dualsense-edge-featured-hardware-image-block-01-en-11aug23.webp",
+  },
+  {
+    id: 6,
+    name: "HD Camera",
+    description: "Capture Your Moments in Stunning Clarity",
+    image:
+      "/images/featuredAccessories/hd-camera-featured-hardware-image-block-01-en-11aug23.webp",
+  },
+  {
+    id: 7,
+    name: "Charging Station",
+    description: "Keep Your Controllers Powered Up",
+    image:
+      "/images/featuredAccessories/charging-station-thumbnail-01-en-08sep23.webp",
+  },
+  {
+    id: 8,
+    name: "Vertical Stand",
+    description: "Stylishly Display Your Console",
+    image:
+      "/images/featuredAccessories/vertical-stand-thumbnail-01-en-08sep23.webp",
+  },
+  {
+    id: 9,
+    name: "Gaming Headset",
+    description: "Immerse Yourself in the Game",
+    image:
+      "/images/featuredAccessories/gaming-headset-thumbnail-01-en-08sep23.webp",
+  },
+  {
+    id: 10,
+    name: "Wireless Adapter",
+    description: "Connect Your Devices Wirelessly",
+    image:
+      "/images/featuredAccessories/wireless-adapter-thumbnail-01-en-08sep23.webp",
+  },
+];
+
+export default featuredAccessoriesArr;
