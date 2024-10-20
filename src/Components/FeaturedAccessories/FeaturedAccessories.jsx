eb8f448856db551a28e9d125587e6e9b1f016153
SabaShagidzeMain <sabadoeswebdev@gmail.com>
2024-10-20 13:08:05 +0400

    npm install swiper
    + Created FeaturedAccessories Component

---
+++ src/Components/FeaturedAccessories/FeaturedAccessories.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+
+const FeaturedAccessories = () => {
+  return <div>FeaturedAccessories</div>;
+};
+
+export default FeaturedAccessories;
