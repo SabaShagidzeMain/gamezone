8e0b1b4049041c24055f5efdcaa471303d16cd79
SabaShagidzeMain <sabadoeswebdev@gmail.com>
2025-02-01 22:16:42 +0400

    feat: game detail page to supabase
    changed game detail fetching logic to supabase

--- src/app/[locale]/games/[platform]/[id]/page.jsx
+++ src/app/[locale]/games/[platform]/[id]/page.jsx
@@ -1,33 +1,37 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import axios from "axios";
+import { supabase } from "@/utilities/supabase/supabase";
 import Navbar from "@/Components/Navbar/Navbar";
 import styles from "./gamedetail.module.css";
 import Image from "next/image";
 import GameImageGallery from "@/Components/GameImageGallery/GameImageGallery";
 import GameSpinner from "@/Components/GameSwiper/GameSwiper";
-
 import Footer from "@/Components/Footer/Footer";
 
 const GameDetails = ({ params }) => {
-  const { id } = params;
+  const { id, platform } = params;
   const [game, setGame] = useState(null);
 
   useEffect(() => {
     const fetchGame = async () => {
       try {
-        const response = await axios.get("/api/games");
-        const allGames = response.data;
-        const selectedGame = allGames.find((game) => game.id === parseInt(id));
-        setGame(selectedGame);
+        const { data, error } = await supabase
+          .from("games_admin")
+          .select("*")
+          .eq("id", id)
+          .single();
+        if (error) {
+          throw error;
+        }
+        setGame(data);
       } catch (error) {
         console.error("Error fetching game details:", error);
       }
     };
 
     fetchGame();
-  }, [id]);
+  }, [id, platform]);
 
   if (!game) {
     return <p>Loading...</p>;
@@ -40,35 +44,38 @@ const GameDetails = ({ params }) => {
       <div className={styles.game_detail}>
         <div
           className={styles.game_thumbnail}
-          style={{ backgroundImage: `url(${game.additionalImages[0]})` }}
+          style={{ backgroundImage: `url(${game.additional_images.add1})` }}
         >
           <Image
-            src={`${game.logo}`}
+            src={`${game.main_images.logo}`}
             width={800}
             height={350}
             alt="Logo"
-          ></Image>
+          />
         </div>
         <div className={styles.game_wrapper}>
           <div className={styles.game_top_inner}>
             <div className={styles.game_gallery}>
               <GameImageGallery
-                thumbnail={game.thumbnail}
-                additionalImages={game.additionalImages}
+                thumbnail={game.main_images.thumbnail}
+                additionalImages={
+                  game.additional_images
+                    ? Object.values(game.additional_images)
+                    : []
+                }
               />
             </div>
             <div className={styles.game_detail_info}>
               <div
                 className={styles.game_detail_pic}
-                style={{ backgroundImage: `url(${game.disc})` }}
+                style={{ backgroundImage: `url(${game.main_images.disc})` }}
               ></div>
               <h1>{game.name}</h1>
               <h3>${game.price}</h3>
               <div className={styles.game_detail_text}>
                 <p>Genre: {game.genre}</p>
                 <p>Rating: {game.rating}</p>
-                <p>Release Date: {game.releaseDate}</p>
-                <p>Difficulty: {game.difficulty}</p>
+                <p>Release Date: {game.release_date}</p>
               </div>
               <div className={styles.button_container}>
                 <button>Buy Now</button>
