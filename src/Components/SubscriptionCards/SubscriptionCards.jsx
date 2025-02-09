e3ff320c089794df83dbd9464ed1b9004eaed1b1
SabaShagidzeMain <sabadoeswebdev@gmail.com>
2025-02-09 13:02:24 +0400

    style: subscriptions page rework 1/2

--- src/Components/SubscriptionCards/SubscriptionCards.jsx
+++ src/Components/SubscriptionCards/SubscriptionCards.jsx
@@ -1,101 +1,84 @@
 import React from "react";
-import styles from "./subcards.module.css";
-
 import handleSub from "@/utilities/handleSub/handleSub";
 import Image from "next/image";
 
 const SubscriptionCards = () => {
   return (
-    <div className={styles.subcard_wrapper}>
-      <div className={styles.subcard}>
-        <h2 className={styles.subcard_header}>
-          ESSENTIAL <br /> <span>Membership</span>
+    <div className="flex p-8 justify-center items-center gap-8 mt-4">
+      <div className="flex flex-col justify-evenly items-center text-center p-[0.2rem] bg-[var(--background-color)] shadow-[var(--box-shadow)] w-72 h-[30rem] text-[var(--text-color)] rounded-[5px] gap-2 cursor-pointer [transition:all_0.2s_ease-in-out] hover:scale-[1.02]">
+        <h2 className="text-[1.3rem]">
+          ESSENTIAL <br /> <span className="text-[0.9rem]">Membership</span>
         </h2>
         <Image
-          className={styles.subcard_image}
           src="/images/sub/sub1.png"
           alt="essential"
           width={180}
           height={180}
           quality={100}
         />
-        <p className={styles.subcard_price}>$10.00 / Month</p>
-        <p className={styles.subcard_feat}>Features:</p>
-        <ul className={styles.subcard_list}>
-          <li className={styles.subcard_item}>
-            Early access to discounts and sales
-          </li>
-          <li className={styles.subcard_item}>
-            Monthly gaming and tech newsletter
-          </li>
-          <li className={styles.subcard_item}>discounted digital downloads</li>
+        <p className="font-bold">$10.00 / Month</p>
+        <p className="font-bold">Features:</p>
+        <ul className="list-none flex flex-col gap-2 text-[0.8rem]">
+          <li>Early access to discounts and sales</li>
+          <li>Monthly gaming and tech newsletter</li>
+          <li>discounted digital downloads</li>
         </ul>
-        <span className={styles.subcard_bar} />
+        <span className="h-[0.02rem] w-[90%] bg-[var(--text-color)]" />
         <button
           onClick={() => handleSub("essential")}
-          className={styles.subcard_button}
+          className="h-8 w-40 rounded-[5px] bg-[var(--text-color)] text-[var(--background-color)] font-bold cursor-pointer [transition:all_0.2s_ease-in-out] border-[none] hover:bg-[#efbf04]"
         >
           Subscribe
         </button>
       </div>
-      <div className={styles.subcard}>
-        <h2 className={styles.subcard_header}>
-          EXTRA <br /> <span>Membership</span>
+      <div className="flex flex-col justify-evenly items-center text-center p-[0.2rem] bg-[var(--background-color)] shadow-[var(--box-shadow)] w-72 h-[30rem] text-[var(--text-color)] rounded-[5px] gap-2 cursor-pointer [transition:all_0.2s_ease-in-out] hover:scale-[1.02]">
+        <h2 className="text-[1.3rem]">
+          EXTRA <br /> <span className="text-[0.9rem]">Membership</span>
         </h2>
         <Image
-          className={styles.subcard_image}
           width={180}
           height={180}
           quality={100}
           src="/images/sub/sub2.png"
           alt="essential"
         />
-        <p className={styles.subcard_price}>$20.00 / Month</p>
-        <p className={styles.subcard_feat}>Features:</p>
-        <ul className={styles.subcard_list}>
-          <li className={styles.subcard_item}>
-            One free monthly game download
-          </li>
-          <li className={styles.subcard_item}>15% discount on games</li>
-          <li className={styles.subcard_item}>
-            Exclusive in-game items or DLCs
-          </li>
+        <p className="font-bold">$20.00 / Month</p>
+        <p className="font-bold">Features:</p>
+        <ul className="list-none flex flex-col gap-2 text-[0.8rem]">
+          <li>One free monthly game download</li>
+          <li>15% discount on games</li>
+          <li>Exclusive in-game items or DLCs</li>
         </ul>
-        <span className={styles.subcard_bar} />
+        <span className="h-[0.02rem] w-[90%] bg-[var(--text-color)]" />
         <button
           onClick={() => handleSub("extra")}
-          className={styles.subcard_button}
+          className="h-8 w-40 rounded-[5px]  bg-[var(--text-color)] text-[var(--background-color)] font-bold cursor-pointer [transition:all_0.2s_ease-in-out] border-[none] hover:bg-[#efbf04]"
         >
           Subscribe
         </button>
       </div>
-      <div className={styles.subcard}>
-        <h2 className={styles.subcard_header}>
-          PREMIUM <br /> <span>Membership</span>
+      <div className="flex flex-col justify-evenly items-center text-center p-[0.2rem] bg-[var(--background-color)] shadow-[var(--box-shadow)] w-72 h-[30rem] text-[var(--text-color)] rounded-[5px] gap-2 cursor-pointer [transition:all_0.2s_ease-in-out] hover:scale-[1.02]">
+        <h2 className="text-[1.3rem]">
+          PREMIUM <br /> <span className="text-[0.9rem]">Membership</span>
         </h2>
         <Image
-          className={styles.subcard_image}
           src="/images/sub/sub3.png"
           alt="essential"
           width={180}
           height={180}
           quality={100}
         />
-        <p className={styles.subcard_price}>$30.00 / Month</p>
-        <p className={styles.subcard_feat}>Features:</p>
-        <ul className={styles.subcard_list}>
-          <li className={styles.subcard_item}>
-            Access to a cloud gaming library
-          </li>
-          <li className={styles.subcard_item}>25% discount on high-end tech</li>
-          <li className={styles.subcard_item}>
-            Personalized tech consultations
-          </li>
+        <p className="font-bold">$30.00 / Month</p>
+        <p className="font-bold">Features:</p>
+        <ul className="list-none flex flex-col gap-2 text-[0.8rem]">
+          <li>Access to a cloud gaming library</li>
+          <li>25% discount on high-end tech</li>
+          <li>Personalized tech consultations</li>
         </ul>
-        <span className={styles.subcard_bar} />
+        <span className="h-[0.02rem] w-[90%] bg-[var(--text-color)]" />
         <button
           onClick={() => handleSub("premium")}
-          className={styles.subcard_button}
+          className="h-8 w-40 rounded-[5px]  bg-[var(--text-color)] text-[var(--background-color)] font-bold cursor-pointer [transition:all_0.2s_ease-in-out] border-[none] hover:bg-[#efbf04]"
         >
           Subscribe
         </button>
--- src/app/[locale]/subscription/page.jsx
+++ src/app/[locale]/subscription/page.jsx
@@ -20,14 +20,12 @@ const SubPage = () => {
 
   return (
     <>
-      <Navbar />
-      <div className={styles.subBanner}></div>
-      <div className={styles.subExpl}>
-        <div className={styles.bannerText}>
-          <h2 className={styles.main_header}>
+      <div className="bg-[var(--text-color)] text-[var(--background-color)] w-full h-60 p-4 flex justify-start items-center text-center mt-16">
+        <div className="flex h-full w-full flex-col justify-center items-center gap-4">
+          <h2 className="text-[1rem] md:text-[1.7rem]">
             Unlock Exclusive Benefits with Our Membership Subscriptions!
           </h2>
-          <p className={styles.main_paragraph}>
+          <p className="w-[80%] text-[0.6rem] md:w-[60%] md:text-[0.8rem]">
             Join our community and get access to exclusive discounts, free
             games, early releases, and more. Whether you`re a casual gamer or a
             tech enthusiast, we have a subscription tailored to suit your needs.{" "}
@@ -40,54 +38,54 @@ const SubPage = () => {
         </div>
       </div>
       <SubscriptionCards />
-      <div className={styles.sub_points}>
-        <h2 className={styles.points_header}>
+      <div className="text-[var(--text-color)] flex justify-center items-center m-4 p-4 flex-col gap-8">
+        <h2 className="font-bold text-[0.9rem]">
           Join the #1 Gaming & Tech Subscription Service Today And Unlock
           Exclusive Perks
         </h2>
-        <div className={styles.point_wrapper}>
-          <div className={styles.point_card}>
-            <div className={styles.point_icon}>
+        <div className="flex gap-8">
+          <div className="text-[var(--text-color)] w-80 text-center flex flex-col gap-[0.8rem] justify-center items-center">
+            <div className="bg-[var(--text-color)] text-[var(--background-color)] w-28 h-28 rounded-[50%] flex justify-center items-center text-[4rem] font-bold">
               <FaRegHandshake />
             </div>
-            <p className={styles.point_header}>
+            <p className="text-[var(--text-color)] w-80 text-center flex flex-col gap-[0.8rem] justify-center items-center">
               Thousands Of Happy Gamers Served
             </p>
-            <p className={styles.point_paragraph}>
+            <p className="text-[0.8rem]">
               We`ve delivered over 10,000 subscription packs filled with games,
               discounts and exclusive persk to passionate gamers nationwide .
             </p>
           </div>
-          <div className={styles.point_card}>
-            <div className={styles.point_icon}>
+          <div className="text-[var(--text-color)] w-80 text-center flex flex-col gap-[0.8rem] justify-center items-center">
+            <div className="bg-[var(--text-color)] text-[var(--background-color)] w-28 h-28 rounded-[50%] flex justify-center items-center text-[4rem] font-bold">
               <IoMdCheckmarkCircleOutline />
             </div>
-            <p className={styles.point_header}>Always Fresh Content</p>
-            <p className={styles.point_paragraph}>
+            <p className="text-[var(--text-color)] w-80 text-center flex flex-col gap-[0.8rem] justify-center items-center">Always Fresh Content</p>
+            <p className="text-[0.8rem]">
               With our subscriptions, you’ll discover new games, exclusive
               deals, and innovative tech every month—no repeats, just fresh,
               exciting experiences tailored for you.
             </p>
           </div>
         </div>
-        <div className={styles.point_wrapper}>
-          <div className={styles.point_card}>
-            <div className={styles.point_icon}>
+        <div className="flex gap-8">
+          <div className="text-[var(--text-color)] w-80 text-center flex flex-col gap-[0.8rem] justify-center items-center">
+            <div className="bg-[var(--text-color)] text-[var(--background-color)] w-28 h-28 rounded-[50%] flex justify-center items-center text-[4rem] font-bold">
               <TbMessage2Share />
             </div>
-            <p className={styles.point_header}>Exclusive Community Access</p>
-            <p className={styles.point_paragraph}>
+            <p className="text-[var(--text-color)] w-80 text-center flex flex-col gap-[0.8rem] justify-center items-center">Exclusive Community Access</p>
+            <p className="text-[0.8rem]">
               Be part of a thriving community of like-minded gamers and tech
               lovers. Share your experiences, join events, and connect with
               others who share your passion for gaming.
             </p>
           </div>
-          <div className={styles.point_card}>
-            <div className={styles.point_icon}>
+          <div className="text-[var(--text-color)] w-80 text-center flex flex-col gap-[0.8rem] justify-center items-center">
+            <div className="bg-[var(--text-color)] text-[var(--background-color)] w-28 h-28 rounded-[50%] flex justify-center items-center text-[4rem] font-bold">
               <GoUnlock />
             </div>
-            <p className={styles.point_header}>No Lock-In Contracts</p>
-            <p className={styles.point_paragraph}>
+            <p className="text-[var(--text-color)] w-80 text-center flex flex-col gap-[0.8rem] justify-center items-center">No Lock-In Contracts</p>
+            <p className="text-[0.8rem]">
               Enjoy the freedom of Pay-By-The-Month with no strings attached.
               You can cancel or pause your subscription anytime, giving you
               complete control over your membership.
@@ -95,10 +93,10 @@ const SubPage = () => {
           </div>
         </div>
       </div>
-      <div className={styles.learn_header}>
-        <span className={styles.sub_span} />
+      <div className="flex flex-col justify-center items-center mx-[0] my-8 text-[var(--text-color)] gap-[0.8rem]">
+        <span className="h-[0.08rem] w-[90%] bg-[var(--text-color)]" />
         <h3>Learn More</h3>
-        <span className={styles.sub_span} />
+        <span className="h-[0.08rem] w-[90%] bg-[var(--text-color)]" />
       </div>
       <SubInfo />
     </>
